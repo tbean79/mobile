a853e2e0a6875e7bdc06910a0dfa23582831f66a
Tbean79 <deant4@byu.edu>
2021-03-18 23:09:52 -0600

    fragment setup

---
+++ app/src/main/java/com/example/mobile/view/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mobile.view;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mobile.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.browse);
+    }
+
+    BrowseFragment browseFragment = new BrowseFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.browse:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, browseFragment).commit();
+                return true;
+
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, profileFragment).commit();
+                return true;
+        }
+        return false;
+    }
+}
